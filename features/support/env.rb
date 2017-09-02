b8c0a1ac9bf50270881a8c45bf1067bb335021e7
kworrad <kmworrad@uwaterloo.ca>
2017-09-01 22:22:06 -0400

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,23 @@
-## env file
+require 'rubygems'
+require 'appium_lib'
+
+desired_caps = {
+  caps: {
+    platformName: 'Android',
+    deviceName: 'Samsung SM-N900W8 (Android 4.4.2, API 19)',
+    appPackage: 'com.dubdub.dubcandy',
+    appActivity: 'com.dubrise.dubcash.splash.view.SplashActivity',
+    noReset: 'true',
+    platformVersion: '4.4.2',
+    exported: 'true'
+  }
+}
+
+Before do
+  $driver = Appium::Driver.new(desired_caps)
+  $driver.start_driver
+end
+
+After do
+  $driver.driver_quit
+end
